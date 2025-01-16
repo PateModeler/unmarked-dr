4192c452c09e888cbdeae7fe6d8c1cc042cdef5c
PateModeler <rottapate@icloud.com>
2025-01-16 12:18:39 +0200

    Add files via upload

---
+++ ntm-drafter/fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'cerulean'
+game { 'gta5' }
+author 'North Team Modding'
+description 'Drafter Poliisi by Pate'
+
+escrow_ignore {
+	'*.meta',
+	'stream/*.ytd'
+}
+
+files {
+    'handling.meta',
+    'vehiclelayouts.meta',
+    'vehicles.meta',
+    'carvariations.meta',
+    'carcols.meta',
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+dependency '/assetpacks'
